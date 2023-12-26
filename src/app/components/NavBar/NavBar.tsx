298fe45155f224ac8ad5245a58fed6c13d42131a
Thanadetch Lertwuthikarn <tlertwuthikarn@palo-it.com>
2023-12-26 23:34:33 +0700

    update navBar

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -2,15 +2,20 @@
 
 import {Button} from "antd";
 import {useRouter} from "@/lib/navigation";
+import {useScrollPosition} from "@/app/hooks/useScrollPosition";
 
 export const NavBar = () => {
+    const scrollPosition = useScrollPosition();
     const router = useRouter();
     const clickHandler = () => {
         router.push("/");
     };
 
     return (
-        <div className="navbar bg-base-100 sticky top-0 z-10">
+        <div className="navbar bg-base-100 sticky top-0 z-10 transition-all ease-in-out duration-300"
+             style={{
+                 boxShadow: scrollPosition > 0 ? "0px 4px 16px 0px rgba(0, 0, 0, 0.10)" : "none",
+             }}>
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl text-primary" onClick={clickHandler}>Life Property</a>
             </div>
